86f5fe29792a91bb8741f4edf2f9bcbc56578a4e
Adegbola Emmanuel <princeadekunle2001@gmail.com>
2026-08-30 22:15:28 +0000

    update on the security config

--- src/main/java/com/example/eCommerce/configuration/SecurityConfig.java
+++ src/main/java/com/example/eCommerce/configuration/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig {
                                 "/api/payments/webhook",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html")
+                                "/swagger-ui.html",
+                                "/api/users/**")
                                 .permitAll()
                                 .anyRequest().authenticated())
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
